67e28969884c78b5713a66dc2297d0074fe3b223
NXM-PC-003\Admin <youssef.manouare@neoxia.com>
2017-06-29 20:25:05 +0000

    test

--- app/src/main/java/com/example/hppc/resto/MainActivity.java
+++ app/src/main/java/com/example/hppc/resto/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+//test
             }
         });
 
